52f75507def3a1e13ba10974d7a388775d5e711f
nik-hil <nikhil.ikhar@gmail.com>
2021-03-31 18:43:06 +0530

    Added User model
    This works with MySQL. It should work with Sqlite.
    
    Demos how to use query sets.

--- models/user.go
+++ models/user.go
@@ -1,86 +1,98 @@
 package models
 
 import (
-	"errors"
-	"strconv"
-	"time"
-)
+	"fmt"
 
-var (
-	UserList map[string]*User
-)
+	"github.com/beego/beego/v2/client/orm"
 
-func init() {
-	UserList = make(map[string]*User)
-	u := User{"user_11111", "astaxie", "11111", Profile{"male", 20, "Singapore", "astaxie@gmail.com"}}
-	UserList["user_11111"] = &u
-}
+	// beego "github.com/beego/beego/v2/server/web"
+	// _ "github.com/mattn/go-sqlite3" // import your required driver
+	_ "github.com/go-sql-driver/mysql"
+)
 
 type User struct {
-	Id       string
-	Username string
-	Password string
-	Profile  Profile
+	Id       int64
+	Username string `orm:"size(100)"`
+	Password string `orm:"size(100)"`
+	Email    string `orm:"unique"`
 }
 
 type Profile struct {
-	Gender  string
-	Age     int
-	Address string
-	Email   string
+	Id      int64  `orm:"auto"`
+	Gender  string `orm:"size(10)"`
+	Age     int64
+	Address string `orm:"size(100)"`
+
+	User *User `orm:"rel(fk)"`
 }
 
-func AddUser(u User) string {
-	u.Id = "user_" + strconv.FormatInt(time.Now().UnixNano(), 10)
-	UserList[u.Id] = &u
-	return u.Id
+func init() {
+	orm.RegisterModel(new(User))
+	orm.RegisterModel(new(Profile))
+	// set default database
+	// orm.RegisterDriver("sqlite", orm.DRSqlite)
+	// orm.RegisterDataBase("default", "sqlite3", "my_db")
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:root@/my_db?charset=utf8")
+	orm.RunSyncdb("default", false, true)
+
+	fmt.Println("In models.init")
 }
 
-func GetUser(uid string) (u *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		return u, nil
+func GetUser(uid int64) (*User, error) {
+	o := orm.NewOrm()
+	user := User{Id: uid}
+	err := o.Read(&user)
+	if err != nil {
+		return nil, err
+	} else {
+		return &user, nil
 	}
-	return nil, errors.New("User not exists")
+}
+func AddUser(user *User) (int64, error) {
+	o := orm.NewOrm()
+
+	id, err := o.Delete(user)
+
+	return id, err
+
 }
 
-func GetAllUsers() map[string]*User {
-	return UserList
-}
-
-func UpdateUser(uid string, uu *User) (a *User, err error) {
-	if u, ok := UserList[uid]; ok {
-		if uu.Username != "" {
-			u.Username = uu.Username
-		}
-		if uu.Password != "" {
-			u.Password = uu.Password
-		}
-		if uu.Profile.Age != 0 {
-			u.Profile.Age = uu.Profile.Age
-		}
-		if uu.Profile.Address != "" {
-			u.Profile.Address = uu.Profile.Address
-		}
-		if uu.Profile.Gender != "" {
-			u.Profile.Gender = uu.Profile.Gender
-		}
-		if uu.Profile.Email != "" {
-			u.Profile.Email = uu.Profile.Email
-		}
-		return u, nil
+func GetAllUsers() (num int64, users []*User, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable("user")
+	num, err = qs.All(&users)
+	if err != nil {
+		return num, nil, err
+	} else {
+		return num, users, nil
 	}
-	return nil, errors.New("User Not Exist")
 }
 
-func Login(username, password string) bool {
-	for _, u := range UserList {
-		if u.Username == username && u.Password == password {
-			return true
-		}
-	}
-	return false
+func UpdateUser(user *User) (int64, error) {
+	o := orm.NewOrm()
+
+	id, err := o.Update(user)
+
+	return id, err
+
+}
+func DeleteUser(user *User) (int64, error) {
+	o := orm.NewOrm()
+
+	id, err := o.Update(user)
+
+	return id, err
+
 }
 
-func DeleteUser(uid string) {
-	delete(UserList, uid)
-}
+// var UserList map[int]*User
+
+// func GetAllUsers() map[int]*User {
+// 	o := orm.NewOrm()
+// 	_, err := o.QueryTable("users").Values(&UserList)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return UserList
+// }
--- routers/router.go
+++ routers/router.go
@@ -8,23 +8,17 @@
 package routers
 
 import (
-	"apiproject/controllers"
-
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
+	
+		// beego.NSNamespace("/user",
+		// 	beego.NSInclude(
+		// 		&controllers.UserController{},
+		// 	),
+		// ),
 	)
 	beego.AddNamespace(ns)
 }
