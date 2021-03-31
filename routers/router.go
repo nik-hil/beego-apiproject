86f729a4e2e69e293f88b086857866c9fd421f14
nik-hil <nikhil.ikhar@gmail.com>
2021-03-31 19:52:56 +0530

    enabled router

--- routers/router.go
+++ routers/router.go
@@ -8,17 +8,19 @@
 package routers
 
 import (
+	"apiproject/controllers"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
 	ns := beego.NewNamespace("/v1",
-	
-		// beego.NSNamespace("/user",
-		// 	beego.NSInclude(
-		// 		&controllers.UserController{},
-		// 	),
-		// ),
+
+		beego.NSNamespace("/user",
+			beego.NSInclude(
+				&controllers.UserController{},
+			),
+		),
 	)
 	beego.AddNamespace(ns)
 }
